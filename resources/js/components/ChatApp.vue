e3f1945b889552c8da84fb33c5f86cbff28b4a95
Rakesh G N <rakeshgn@ideacarve.com>
2019-01-10 03:13:37 +0530

    inital commit, project added, migrations created, factory created

---
+++ resources/js/components/ChatApp.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="chat-app">
+    <Conversation :contact="selectedContact" :messages="messages"/>
+    <Contactlist :contacts="contacts"/>
+  </div>
+</template>
+
+<script>
+    import Conversation from './Conversation';
+    import Contactlist from './Contactlist';
+    export default {
+
+        props:{
+            user: {
+                type: Object,
+                required : true
+            }
+        },
+        data(){
+        return {
+        selectedContact:null,
+        messages: [],
+        contacts: []
+        }
+        },
+        mounted() {
+            console.log(this.user);
+            axios.get('/contacts')
+            .then((response) => {
+                console.log(response.data);
+                this.contacts = response.data
+            })
+        },
+        components: {Conversation,Contactlist}
+    }
+</script>
